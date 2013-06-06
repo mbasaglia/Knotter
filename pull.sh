787a69fd47d6cbbac694b6a5f1a8c7cdd659da91
Mattia Basaglia <mattia.basaglia@gmail.com>
2013-06-06 09:19:38 +0200

    Pull helper script

--- pull.sh
+++ pull.sh
@@ -1,4 +1,5 @@
 #!/bin/bash 
 
 # script to update all code in one command
-git pull && git submodule update --init && git submodule foreach git pull origin master 
+git pull origin master && git submodule update --init \
+    && git submodule foreach git pull origin master 
